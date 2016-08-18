cff79efdc3a6e2d5d23ceab8d24acd69e138ee49
Ismael Sanchez <ismaelsanchez@Ismaels-MacBook-Pro-3.local>
2016-08-17 23:14:51 -0700

    finished product

--- titleCaseASentence/titleCaseASentence.js
+++ titleCaseASentence/titleCaseASentence.js
@@ -1,19 +1,26 @@
 function titleCase(str) {
-  var word,char,thar = [];
-  word = str.split(' ');
-   for (var i = 0; i<word.length; i ++){    
-    char = word[i].split('');
-    var toc = char[0];
-    check = toc.toUpperCase();
-    char.shift();
-    char.unshift(check);
-    var check = char.join('');
-    thar.push(check);
-    str = thar.join(' ');
-    
-   }
+  var word,letter,thar = [];
+  var check, slow, mic = [];
+  var toc, res = ('');              // define my variables to empty arrays and strings
   
-  return str;
+  word = str.split(' ');            // takes a string and splits the string every case that at space is encountered (individual words)
+   
+  for (var i = 0; i<word.length; i ++){    // for loop that manipulates each individua word 
+    letter = word[i].split('');      // takes a word and splits it into individual characters
+    toc = letter[0];                 // isolates the first character of each word
+    check = toc.toUpperCase();     // capitalizes the first letter of the word
+    letter.shift();                  // takes the uncapitalized letter in the array and leaves the rest of the array still in tact
+    mic = letter.join('');            // makes the remaining letters in the array into a string
+    res = mic.toLowerCase();       // makes the string of the former array all lower case
+    slow = res.split('');          // places the new lower case string into a new array
+    slow.unshift(check);           // places the capialized first letter of the original word back to the front of the array
+    check = slow.join('');         // makes the newly formed array a string
+    thar.push(check);              // places the new word into another array with the word fully manipulated and in tact
+    str = thar.join(' ');          // places the words together to form asentence that has all of the words first letter capitalized and the rest of the letters lower case
+    }
+  
+  return str;                     // returns the anticipated result
 }
 
-titleCase("I'm a little tea pot");
+titleCase("I'm a liTTle tea pot");
+
